9ece944a4d37a13c0b789f451215428855020122
lucky <lijicai0468@126.com>
2019-01-15 17:57:58 +0800

    add function

--- gPath/path.go
+++ gPath/path.go
@@ -9,13 +9,17 @@ import (
 	"strings"
 )
 
-//当前目录
+//current path
 func Current() string {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0])) //返回绝对路径  filepath.Dir(os.Args[0])去除最后一个元素的路径
 	if err != nil {
 		log.Fatal(err)
 	}
 	return strings.Replace(dir, "\\", "/", -1) //将\替换成/
+}
+
+func CurrentFile(s string) string {
+	return Current() + "/" + s
 }
 
 func BaseLog() string {
